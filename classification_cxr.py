51e3b1c66d07fb59dd5db73f9a95f571dd13fac3
Harish Anand <harishanand95@gmail.com>
2017-03-29 23:36:16 +0530

    Added a 2 layer CNN tensorflow model.  Accuracy obtained ranges at 45% =====  Verified

--- classification_cxr.py
+++ classification_cxr.py
@@ -5,26 +5,26 @@ import numpy as np
 import matplotlib.image as mpimg
 from scipy import misc
 from six.moves import cPickle as pickle
+import tensorflow as tf
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# s = {}
+# files = os.listdir(dir_path + "/ClinicalReadings")
+# d = {}
+# for i in files:
+# 	with open(dir_path+"/ClinicalReadings/%s" % i, "r") as text_file:
+# 		lines = text_file.readlines()
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-s = {}
-files = os.listdir(dir_path + "/ClinicalReadings")
-d = {}
-for i in files:
-	with open(dir_path+"/ClinicalReadings/%s" % i, "r") as text_file:
-		lines = text_file.readlines()
+# 	if len(lines) > 2 :
+# 		d[i] = lines
 
-	if len(lines) > 2 :
-		d[i] = lines
-
-	if lines[1] in s:
-		s[lines[1]] = s[lines[1]] + 1
-	else:
-		s[lines[1]] = 1
-print "=== S ==="
-pprint.pprint(s)
-print "=== D ==="
-pprint.pprint(d)
+# 	if lines[1] in s:
+# 		s[lines[1]] = s[lines[1]] + 1
+# 	else:
+# 		s[lines[1]] = 1
+# print "=== S ==="
+# pprint.pprint(s)
+# print "=== D ==="
+# pprint.pprint(d)
 
 
 
@@ -97,8 +97,8 @@ def get_dataset():
 		data = pickle.load(f)
 		dataset = data["dataset"]
 		labels = data["labels"]
-		print "===LABELS==="
-		print labels
+		# print "===LABELS==="
+		# print labels
 		## remove 182
 		valid_dataset = np.ndarray(shape=(182, image_width, image_height),
 							 dtype=np.float32)
@@ -141,13 +141,10 @@ def get_dataset():
 
 train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels = get_dataset()
 
-
 num_labels = 2
 def reformat(dataset, labels):
-	dataset = dataset.reshape((-1, image_width * image_height)).astype(np.float32)
-	# Map 2 to [0.0, 1.0, 0.0 ...], 3 to [0.0, 0.0, 1.0 ...]
+	dataset = dataset.reshape((-1, image_width, image_height, 1)).astype(np.float32)
 	labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
-	print labels
 	return dataset, labels
 
 train_dataset, train_labels = reformat(train_dataset, train_labels)
@@ -156,3 +153,73 @@ test_dataset, test_labels = reformat(test_dataset, test_labels)
 print('Training set', train_dataset.shape, train_labels.shape)
 print('Validation set', valid_dataset.shape, valid_labels.shape)
 print('Test set', test_dataset.shape, test_labels.shape)
+
+
+def accuracy(predictions, labels):
+	return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
+
+batch_size = 16
+patch_size = 5
+depth = 16
+num_hidden = 64
+num_channels = 1
+graph = tf.Graph()
+
+with graph.as_default():
+	# Input data.
+	tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_width, image_height, num_channels))
+	tf_train_labels  = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+	tf_valid_dataset = tf.constant(valid_dataset)
+	tf_test_dataset  = tf.constant(test_dataset)
+
+	# Variables.
+	layer1_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, num_channels, depth], stddev=0.1))
+	layer1_biases  = tf.Variable(tf.zeros([depth]))
+	layer2_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, depth, depth], stddev=0.1))
+	layer2_biases  = tf.Variable(tf.constant(1.0, shape=[depth]))
+	layer3_weights = tf.Variable(tf.truncated_normal([image_width // 4 * image_height // 4 * depth, num_hidden], stddev=0.1))
+	layer3_biases  = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+	layer4_weights = tf.Variable(tf.truncated_normal([num_hidden, num_labels], stddev=0.1))
+	layer4_biases  = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+
+	# Model.
+	def model(data):
+		conv    = tf.nn.conv2d(data, layer1_weights, [1, 2, 2, 1], padding='SAME')
+		hidden  = tf.nn.relu(conv + layer1_biases)
+		conv    = tf.nn.conv2d(hidden, layer2_weights, [1, 2, 2, 1], padding='SAME')
+		hidden  = tf.nn.relu(conv + layer2_biases)
+		shape   = hidden.get_shape().as_list()
+		reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+		hidden  = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+		return tf.matmul(hidden, layer4_weights) + layer4_biases
+
+	# Training computation.
+	logits = model(tf_train_dataset)
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
+
+	# Optimizer.
+	optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+
+	# Predictions for the training, validation, and test data.
+	train_prediction = tf.nn.softmax(logits)
+	valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+	test_prediction  = tf.nn.softmax(model(tf_test_dataset))
+
+
+num_steps = 200
+
+with tf.Session(graph=graph) as session:
+	tf.initialize_all_variables().run()
+	print('Initialized')
+	for step in range(num_steps):
+		offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+		batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+		batch_labels = train_labels[offset:(offset + batch_size), :]
+		feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+		_, l, predictions = session.run(
+			[optimizer, loss, train_prediction], feed_dict=feed_dict)
+		if (step % 50 == 0):
+			print('Minibatch loss at step %d: %f' % (step, l))
+			print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+			print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), valid_labels))
+	print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
