61c4404529d0a7d408f57106d1f7edf60c0bc954
Harish Anand <harishanand95@gmail.com>
2017-04-07 20:31:36 +0530

    Added name space to conv and fc layers for better display in Tensorboard.  Removed vgg test run code  ==   Verified.

--- classification_cxr.py
+++ classification_cxr.py
@@ -6,29 +6,6 @@ import matplotlib.image as mpimg
 from scipy import misc
 from six.moves import cPickle as pickle
 import tensorflow as tf
-from vgg import vgg_16
-import inception_resnet_v2
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# s = {}
-# files = os.listdir(dir_path + "/ClinicalReadings")
-# d = {}
-# for i in files:
-# 	with open(dir_path+"/ClinicalReadings/%s" % i, "r") as text_file:
-# 		lines = text_file.readlines()
-
-# 	if len(lines) > 2 :
-# 		d[i] = lines
-
-# 	if lines[1] in s:
-# 		s[lines[1]] = s[lines[1]] + 1
-# 	else:
-# 		s[lines[1]] = 1
-# print "=== S ==="
-# pprint.pprint(s)
-# print "=== D ==="
-# pprint.pprint(d)
-
-
 
 
 def rgb2gray(rgb):
@@ -73,7 +50,7 @@ def load_images(folder, min_num_images):
 	labels = labels[0:num_images]
 	if num_images < min_num_images:
 	  raise Exception('Many fewer images than expected: %d < %d' %
-	                  (num_images, min_num_images))
+					  (num_images, min_num_images))
 
 	print('Full dataset tensor:', dataset.shape)
 	print('Mean:', np.mean(dataset))
@@ -158,7 +135,10 @@ print('Test set', test_dataset.shape, test_labels.shape)
 def accuracy(predictions, labels):
 	# shape   = tf.convert_to_tensor(predictions).get_shape().as_list()
 	# prediction = tf.reshape(predictions, [shape[0] * shape[1] * shape[2], shape[3]])
-	return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
+	with tf.name_scope("accuracy"):
+		accuracy = (100.0 * np.sum(np.argmax(predictions , 1) == np.argmax(labels, 1)) / predictions.shape[0])
+		tf.summary.scalar("accuracy", accuracy)
+		return accuracy
 
 batch_size = 16
 patch_size = 5
@@ -169,74 +149,69 @@ num_channels = 1
 graph1 = tf.Graph()
 with graph1.as_default():
 	# Input data.
-	tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_width, image_height, num_channels))
-	tf_train_labels  = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+	tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_width, image_height, num_channels),name="input")
+	tf_train_labels  = tf.placeholder(tf.float32, shape=(batch_size, num_labels),name="labels")
 	tf_valid_dataset = tf.constant(valid_dataset)
 	tf_test_dataset  = tf.constant(test_dataset)
 
-	# Variables.
-	layer1_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, num_channels, depth], stddev=0.1))
-	layer1_biases  = tf.Variable(tf.zeros([depth]))
-	layer2_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, depth, depth], stddev=0.1))
-	layer2_biases  = tf.Variable(tf.constant(1.0, shape=[depth]))
-	layer3_weights = tf.Variable(tf.truncated_normal([image_width // 4 * image_height // 4 * depth, num_hidden], stddev=0.1))
-	layer3_biases  = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
-	layer4_weights = tf.Variable(tf.truncated_normal([num_hidden, num_labels], stddev=0.1))
-	layer4_biases  = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+	def conv(data, patch_size, num_channels, depth, name="conv"):
+		with tf.name_scope(name):
+			W = tf.Variable(tf.truncated_normal([patch_size, patch_size, num_channels, depth], stddev=0.1),name="W")
+			B  = tf.Variable(tf.zeros([depth]),name="B")
+			conv    = tf.nn.conv2d(data, W, [1, 2, 2, 1], padding='SAME') # zero padded to keep ratio same
+			activation  = tf.nn.relu(conv + B)
+			tf.summary.histogram("weights", W)
+			tf.summary.histogram("biases", B)
+			tf.summary.histogram("activation", activation)
+			return activation
 
-	# Model.
+# [image_width // 4 * image_height // 4 * depth, num_hidden]
+# [num_hidden, num_labels]
+
+	def fc_layer(data, width, height, name="fc"):
+		with tf.name_scope(name):
+			W = tf.Variable(tf.truncated_normal([width, height], stddev=0.1),name="W")
+			B  = tf.Variable(tf.constant(1.0, shape=[height]),name="B")
+			mul = tf.matmul(data,W)
+			activation  = tf.nn.relu(mul + B)
+			tf.summary.histogram("weights", W)
+			tf.summary.histogram("biases", B)
+			tf.summary.histogram("activation", activation)
+			return activation
+
 	def model(data):
-		conv    = tf.nn.conv2d(data, layer1_weights, [1, 2, 2, 1], padding='SAME')
-		hidden  = tf.nn.relu(conv + layer1_biases)
-		conv    = tf.nn.conv2d(hidden, layer2_weights, [1, 2, 2, 1], padding='SAME')
-		hidden  = tf.nn.relu(conv + layer2_biases)
-		shape   = hidden.get_shape().as_list()
-		reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
-		hidden  = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
-		return tf.matmul(hidden, layer4_weights) + layer4_biases
-
+		conv1 = conv(data, patch_size, num_channels, depth, name="conv1")
+		conv2 = conv(conv1, patch_size, depth, depth, name="conv2")
+		shape   = conv2.get_shape().as_list()
+		reshape = tf.reshape(conv2, [shape[ 0], shape[1] * shape[2] * shape[3]], name="reshape_fc")
+		fc1 = fc_layer(reshape, image_width // 4 * image_height // 4 * depth, num_hidden, name="fc1")
+		fc2 = fc_layer(fc1, num_hidden, num_labels, name="fc2")
+		return fc2
 	# Training computation.
 	logits = model(tf_train_dataset)
-	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
-
+	with tf.name_scope("loss"):
+		loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
+	tf.summary.scalar("loss", loss)
 	# Optimizer.
-	optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+	with tf.name_scope("optimiser"):
+		optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
 
 	# Predictions for the training, validation, and test data.
-	train_prediction = tf.nn.softmax(logits)
-
-	valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
-
-	test_prediction = tf.nn.softmax(model(tf_test_dataset))
-
-# VGGNET-16 Implementation
-
-# graph2 = tf.Graph();
-# with graph2.as_default():
-# 	# Input data.
-# 	tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_width, image_height, num_channels))
-# 	tf_train_labels  = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
-# 	tf_valid_dataset = tf.constant(valid_dataset)
-# 	tf_test_dataset  = tf.constant(test_dataset)
-
-# 	logits ,_ = vgg_16(tf_train_dataset, num_classes=2, spatial_squeeze=False)
-# 	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
-# 	optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
-# 	train_prediction = tf.nn.softmax(logits)
-
-# 	with tf.variable_scope("g2valid") as scope:
-# 		_1, _ = vgg_16(tf_valid_dataset, num_classes=2, is_training=False, spatial_squeeze=False)
-# 	valid_prediction = tf.nn.softmax(_1)
-
-# 	with tf.variable_scope("g2test") as scope:
-# 		_2, _ = vgg_16(tf_test_dataset, num_classes=2, is_training=False, spatial_squeeze=False)
-# 	test_prediction  = tf.nn.softmax(_2)
+	with tf.name_scope("train"):
+		train_prediction = tf.nn.softmax(logits)
+	with tf.name_scope("valid"):
+		valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+	with tf.name_scope("test"):
+		test_prediction = tf.nn.softmax(model(tf_test_dataset))
+	summ = tf.summary.merge_all()
 
 num_steps = 20
 def run_training(graph):
 	with tf.Session(graph=graph) as session:
 		tf.initialize_all_variables().run()
-		# summary_writer = tf.summary.FileWriter('log_simple_stats', session.graph)
+		writer = tf.summary.FileWriter('/tmp/log_simple_stats/3')
+		writer.add_graph(session.graph)
+
 		print('Initialized')
 		for step in range(num_steps):
 			offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
@@ -245,10 +220,10 @@ def run_training(graph):
 			feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
 			_, l, predictions = session.run(
 				[optimizer, loss, train_prediction], feed_dict=feed_dict)
-			print "Prediction" 
-			print predictions
-			print "Values" 
-			print batch_labels
+			# print "Prediction" 
+			# print predictions
+			# print "Values" 
+			# print batch_labels
 			# summary_writer.add_summary(session.run(summaries), step)
 			if (step % 5 == 0):
 				print('Minibatch loss at step %d: %f' % (step, l))
